93af34cf6f43e5b697413519c676e08f6decbf52
Vladimir Korobov <3573333@gmail.com>
2026-03-15 22:52:28 +0300

    Improved Swagger documentation for the product response & pagination

--- products-service/src/products/dto/paginated-products-response.dto.ts
+++ products-service/src/products/dto/paginated-products-response.dto.ts
@@ -1,19 +1,34 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { ProductResponseDto } from './product-response.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { ProductResponseDto } from "./product-response.dto";
 
 export class PaginatedProductsResponseDto {
-  @ApiProperty({ type: () => [ProductResponseDto] })
+  @ApiProperty({
+    type: () => [ProductResponseDto],
+    description: "List of products for the current page",
+  })
   items!: ProductResponseDto[];
 
-  @ApiProperty({ example: 125 })
+  @ApiProperty({
+    example: 125,
+    description: "Total number of products",
+  })
   total!: number;
 
-  @ApiProperty({ example: 1 })
+  @ApiProperty({
+    example: 1,
+    description: "Current page number",
+  })
   page!: number;
 
-  @ApiProperty({ example: 10 })
+  @ApiProperty({
+    example: 10,
+    description: "Maximum products per page",
+  })
   limit!: number;
 
-  @ApiProperty({ example: 13 })
+  @ApiProperty({
+    example: 13,
+    description: "Total number of available pages",
+  })
   totalPages!: number;
 }
